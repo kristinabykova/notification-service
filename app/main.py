e0255459c2d43a57fcb57a96dfba82c96bd46f6b
kristinabykova <kristina.bykova.03@list.ru>
2026-05-09 17:50:04 +0300

    fix main

--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ import uvicorn
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def root():
     return {"status": "ok"}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    app.run(debug=True, host="127.0.0.1", port=5000)
